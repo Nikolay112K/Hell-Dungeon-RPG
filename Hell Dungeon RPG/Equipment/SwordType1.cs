157604e4366fd3f13467fe42e7f3e3bf0c435f05
DESKTOP-QFTHJVS\Nik <kolya-kirich@i.ua>
2021-03-03 00:21:09 +0100

    1

--- Hell Dungeon RPG/Equipment/SwordType1.cs
+++ Hell Dungeon RPG/Equipment/SwordType1.cs
@@ -7,7 +7,7 @@ namespace Hell_Dungeon_RPG.Equipment
     public class SwordType1
     {
         private int SwordStrength = 100;
-        private int SwordDamage = 7;
+        private int SwordDamage = 5;
         public SwordType1(int swordStrength,int swordDamage)
         {
             this.SwordDamage = swordDamage;
